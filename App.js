04e5daad13412a0cc56162c269a6b5db69d1caf0
yuna17c <57446370+yuna17c@users.noreply.github.com>
2021-02-05 16:38:17 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, Alert, Image, View, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("text pressed")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native</Text>
+      <TouchableWithoutFeedback onPress={() => Alert.alert("My Title", "My Message",
+       [
+        {text: "Yes"},
+        {text: "No"}
+        ])}>
+        <Image 
+        fadeDuration={1000}
+        source={require('./assets/favicon.png')}
+          />
+      </TouchableWithoutFeedback>
+      <Button 
+      color="orange"
+      title="Click Me" onPress={() => console.log("button clicked")}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +29,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    justifyContent:'center',
   },
 });
+ 
