e5cd28049576a9d4a27b6538c3a53d751642c245
yuna17c <57446370+yuna17c@users.noreply.github.com>
2021-02-15 00:42:37 -0500

    added profile

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,62 +1,34 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { TouchableOpacity, ImageBackground, StyleSheet, Image, Platform, StatusBar, View, Button } from 'react-native';
-=======
-import { TouchableOpacity, Image, ImageBackground, StyleSheet, Platform, StatusBar, View, Button } from 'react-native';
->>>>>>> df963b220ee616e061a51a818fee8ebf2e1b07b3
-=======
-
-import { TouchableOpacity, Image, ImageBackground, StyleSheet, Platform, StatusBar, View, Button } from 'react-native';
->>>>>>> 1acefe5fffd913ef1fbb0c326c2f0d492c3ce987
 
 function HomeScreen({ navigation }) {
     return (
         <ImageBackground
             style={styles.background}
-<<<<<<< HEAD
-<<<<<<< HEAD
             source={require('../assets/homeBackground.png')}
         >
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Login')}
-                style={styles.homeButton}>
-                <Image source={require('../assets/homeButton.png')} />
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                <Image
+                    source={require("../assets/homeButton.png")}
+                    style={styles.homeButton}
+                />
             </TouchableOpacity>
-
-=======
-=======
->>>>>>> 1acefe5fffd913ef1fbb0c326c2f0d492c3ce987
-            source={require('../assets/homeBackground.png')}>
-            
-            <TouchableOpacity onPress={()=>navigation.navigate('Dashboard')}>
-                <Image source={require("../assets/up_arrow.png")}/>
-            </TouchableOpacity>
-            
-            {/* <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Dashboard')}
-            /> */}
-<<<<<<< HEAD
->>>>>>> df963b220ee616e061a51a818fee8ebf2e1b07b3
-=======
->>>>>>> 1acefe5fffd913ef1fbb0c326c2f0d492c3ce987
         </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     background: {
+        width: '100%',
+        height: '100%',
         flex: 1,
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     },
     homeButton: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        marginBottom: 24,
-        alignItems: 'center',
-
+        marginBottom: 35,
     }
 })
 
---
+++ app/screens/Message.js
@@ -0,0 +1,4 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { ImageBackground, Text, contentContainerStyle, ScrollView, Image, StyleSheet, Platform, StatusBar, View, Button, LogBox } from 'react-native';
+import colors from '../config/colors';
