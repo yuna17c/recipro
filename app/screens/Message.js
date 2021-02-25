f99e4e7d8fc194cdfdba7d8b7b4e46d114411304
Jessica <jess94yng@gmail.com>
2021-02-25 01:27:23 -0500

    sendRequest screen finished, message screen finished

--- app/screens/Message.js
+++ app/screens/Message.js
@@ -1,43 +1,56 @@
 import 'react-native-gesture-handler';
 import React, { useRef } from "react";
-import { Animated, Text, View, StyleSheet, Button } from "react-native";
+import { Image, TouchableOpacity, Animated, Text, View, StyleSheet, Button } from "react-native";
 import colors from '../config/colors';
-
 import { render } from 'react-dom';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const Message = () => {
-    // fadeAnim will be used as the value for opacity. Initial Value: 0
-    const fadeAnim = useRef(new Animated.Value(0)).current;
+function Message ({route,navigation}) {
 
-    const fadeIn = () => {
-        // Will change fadeAnim value to 1 in 5 seconds
-        Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 5000
-        }).start();
-    };
-
-    const fadeOut = () => {
-        // Will change fadeAnim value to 0 in 5 seconds
-        Animated.timing(fadeAnim, {
-            toValue: 0,
-            duration: 5000
-        }).start();
-    };
+    const { userValue } = route.params;
 
     return (
-        <View style={styles.container}>
-            <Animated.View
-                style={[
-                    styles.fadingContainer,
-                    { opacity: fadeAnim }
-                ]}
-            >
-                <Text style={styles.fadingText}>Fading View!</Text>
-            </Animated.View>
-            <View style={styles.buttonRow}>
-                <Button title="Fade In" onPress={fadeIn} />
-                <Button title="Fade Out" onPress={fadeOut} />
+        <View style={styles.parentContainer}>
+            <View style={styles.container}>
+                <ScrollView>
+                    <Image source = {require('../assets/message_screen.png')}></Image>
+                </ScrollView>
+            </View>
+            <View style={styles.barContainer}>
+                <View style={styles.buttons}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Dashboard')}
+                        style={styles.workButton}>
+                        <Image
+                            source={require("../assets/dashboardButton.png")}
+                            style={styles.buttonItem}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Work')}
+                        style={styles.workButton}>
+                        <Image
+                            source={require("../assets/workButton.png")}
+                            style={styles.buttonItem}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('SendRequest', {userValue})}
+                        style={styles.sendButton}>
+                        <Image
+                            source={require("../assets/sendButton.png")}
+                            style={styles.buttonItem}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Message')}
+                        style={styles.chatButton}>
+                        <Image
+                            source={require("../assets/chatButton.png")}
+                            style={styles.buttonItem}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
@@ -47,7 +60,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        backgroundColor: colors.cream,
     },
     fadingContainer: {
         paddingVertical: 8,
@@ -62,6 +76,35 @@ const styles = StyleSheet.create({
     buttonRow: {
         flexDirection: "row",
         marginVertical: 16
+    },
+    barContainer: {
+        flex: 0.08,
+        backgroundColor: colors.secondary,
+    },
+    buttons: {
+        width: '95%',
+        alignSelf: 'center',
+        marginTop: 10,
+        flexDirection: 'row',
+    },
+    parentContainer: {
+        flex: 1,
+    },
+    sendButton: {
+        flex: 1,
+    },
+    chatButton: {
+        flex: 1,
+    },
+    workButton: {
+        flex: 1,
+    },
+    buttonItem: {
+        alignSelf: 'center',
+    },
+    imageStyle: {
+        position: 'absolute',
+        marginBottom:20,
     }
 });
 
